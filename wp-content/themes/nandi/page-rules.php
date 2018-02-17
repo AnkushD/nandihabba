2cd32f4febd26cf44f2004d986215da460531d4e
Ankush <ankush25d@gmail.com>
2018-02-17 12:19:45 +0530

    Prize and Rules page added

---
+++ wp-content/themes/nandi/page-rules.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Template Name: Events 
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <div class="n-flat-bar-masthead">
+                <div class="n-flat-bar-inner">
+                    <div class="n-flat-bar-lt">
+                        <h1>Rules and Regulations</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="n-pg-side-padding n-inside-pg-content">
+                <section class="n-inside-pg-section">
+                    <p class="n-block-title margin-bottom-10">Race Regulation</p> 
+                    <div class="n-small-text n-block">
+                        <ul class="list-unstyled">
+                            <li>
+                                <span>You are to participate only in the race category for which you had applied and for which your entry has been confirmed. Your entry and running number bib and/or bib tag is not transferable to any other person under any circumstance</span>
+                            </li>
+                            <li>
+                                <span>Runners found to have interchanged their running number bib and/or bib tag with another individual, will be disqualified from the Event and shall not be allowed to apply or participate</span>
+                            </li>
+                            <li>
+                                <span>The Organizer reserves the right to disqualify or exclude any person from the event/competition who gives incorrect personal data/details on the entry form, who has been banned from competition under IAAF jurisdiction, or who is suspected of having taken banned substances. No refund of entry fee will be made</span>
+                            </li>
+                            <li>
+                                <span>No result or certificate will be given to runners who do not start their run at the respective start times of the race categories they have been confirmed in</span>
+                            </li>
+                            <li>
+                                <span>Please note if you tamper with the bib tag in any way or remove/exchange it with another runner, you won't be able to get an official finish time and thereby the official timing certificate recording your finish times. No Chip, No Timing, No Prize Money</span>
+                            </li>
+                            <li>
+                                <span>No wheeled vehicles are allowed on the course. Anyone bringing wheeled vehicles will be disqualified from the race and asked to leave the course</span>
+                            </li>
+                            <li>
+                                <span>Participants are not allowed to run with pets, unregistered runners or babies/children who are under aged for the designated category</span>
+                            </li>
+                            <li>
+                                <span>Participants will not be allowed to enter the event venue with gas balloons</span>
+                            </li>
+                            <li>
+                                <span>In order to ensure the re-opening of roads by 10.30 a.m. (Or as decided by the event organisers), the organizers will designate cut off times at specified locations. Runners failing to reach these specific locations within times specified must stick to the footpath in order to complete your race</span>
+                            </li>
+                            <li>
+                                <span>Only those confirmed participants wearing the current Nandi Marathon 2018 running number bibs will be allowed on the route. Any person found wearing the incorrect bib or no bib will be disqualified and removed by security</span>
+                            </li>
+                            <li>
+                                <span>Further, every runner must wear his/her running number bib on the front of the vest. Any mutilation, fold, alteration or damage to the bib will amount to disqualification of the runner from the Event</span>
+                            </li>
+                            <li>
+                                <span>Organizers reserve the right to stop any participant from participating who is found medically unfit to continue in the opinion of the medical personnel</span>
+                            </li>
+                            <li>
+                                <span>Baggage counters are available only for participants of the Full Marathon and Half Marathon categories.For security reasons, participants are requested to come, as far as possible, without any baggage. It is strongly recommended that you do not leave valuables like mobile phone, camera, jewellery, watch, electronic gadgets, etc. in the bag stored. Organizers are not responsible for any loss or baggage and belongings</span>
+                            </li>
+                            <li>
+                                <span>Participants are requested to cooperate with the police and security personnel deputed at the event venues and on route. Participants/supporters are liable to random checking on race day this is for your own security</span>
+                            </li>
+                            <li>
+                                <span>Photographs taken of runners during any Cyclothon/Marathon event organised prior to and on race day can be used for the purpose of promoting the event</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">General</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            Use only the application modes suggested on this website to submit your entries. Please thoroughly go through all relevant information given on the event website and application form prior to submitting your application.
+                        </p>
+                    </div>
+                    <p class="n-event-info">
+                        Requests for participation cancellation, and thereby refund, will not be entertained.
+                    </p>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Entries</p> 
+                    <div class="n-small-text n-block">
+                        <ul class="list-unstyled">
+                            <li>
+                                <span>Only individual entries for Marathon, half marathon and 5K   Run categories shall be accepted</span>
+                            </li>
+                            <li>
+                                <span>Half Marathon applicants must be above the age of 18. supporting age proof, such as passport copy, drivers licence or Adhaar card must be carried when collecting the bibs for the event</span>
+                            </li>
+                            <li>
+                                <span>In case of 5K   Run, any minor participant must be accompanied by an adult</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Entry Confirmations</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            Confirmation of applications received will be subject to Entry Rules & Guidelines given herein. Confirmation of marathon and half marathon applications will also be subject to the Confirmation Criteria.
+                        </p>
+                        <p>
+                            Post close of registrations, email confirmations will be sent to all the confirmed participants who have given their valid email id in the application form. Physical acceptance letters will be posted ONLY to those applicants who have not given their email address in the application form. 
+                            <br />
+                            <br />
+                            If you do not receive any intimation from the race office, <br /> please email us at 
+                            <!--email_off-->
+                            <a href="mailto:shankar@nammacycle.in">
+                                shankar@nammacycle.in
+                            </a>
+                            <!--email_off--> 
+                        </p>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Application Status</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            On application being successfully submitted, registrants will receive an email and sms confirming their application with a unique code.
+                        </p>
+                        <p>
+                            Soon after the entries close, runners will receive an email and sms with their BIB nos and other details.
+                        </p>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Winners Medal</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            On the Race Day, victory ceremony will be held only to top 3 overall Elite/fastest finishers in men & women category.
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <span>All age category winners will be announced on our website and on our Social Media Channels</span>
+                            </li>
+                            <li>
+                                <span>They will be provided with their winner’s medals & prize money 45 days from the finish of the event</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Merit Certificate</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            All prize money winners of the run (as given in the prize money structure in prize money tab) will receive Merit Certificates certifying their prize money winning positions.
+                        </p>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Finishers Medal</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            All runners of the Half Marathon, 10K and 5k Run category will get a Finisher's Medal post their run. Medals will not be posted to individual runners.
+                        </p>
+                    </div>
+
+                    <br />
+                    <p class="n-block-title margin-bottom-10">Timing Certificate</p> 
+                    <div class="n-small-text n-block">
+                        <p>
+                            Timing certificates of marathon finishers will be available for download from the event website within 30 days from race day.
+                        </p>
+                    </div>
+                </section>
+            </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
