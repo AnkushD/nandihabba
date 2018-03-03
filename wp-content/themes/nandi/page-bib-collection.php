9db270c1848e8b2b59d368455b6f90af68385993
Ankush <ankush25d@gmail.com>
2018-03-03 15:21:50 +0530

    bibs page added

---
+++ wp-content/themes/nandi/page-bib-collection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Events 
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <div class="n-flat-bar-masthead">
+                <div class="n-flat-bar-inner">
+                    <div class="n-flat-bar-lt">
+                        <h1>BIB COLLECTION</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="n-pg-side-padding n-inside-pg-content">
+                <section class="n-inside-pg-section">
+            
+                    <p>RUNNING BIBs can be collected at St Joseph Primary School.</p>
+                    <p class="margin-bottom-30">CYCLING BIBs will be given tomorrow at the venue.</p>
+                    <p class="n-block-title">BIB collection venue:</p> 
+                    <p>It is located just opposite UB city on Vittal Lallya Road.</p>
+                    <p class="margin-bottom-30">Find the location on the map <a href="https://goo.gl/maps/FkL5n3w7yBL2" target="_blank">https://goo.gl/maps/FkL5n3w7yBL2</a>. </p>
+
+                    <p class="n-block-title">Collection time:</p> 
+                    <p class="margin-bottom-30">11:00 PM to 4:00 PM</p>
+    
+
+                    <p>Contact <span style="color:#FE2568">sunil.nanjappa@gmail.com</span> for any queries regarding running and cycling.</p> 
+                </section>
+            </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
